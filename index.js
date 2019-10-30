b538c3ee657d469b5b34a47d217dfd4e4f922aca
juliendubrail <dubrail.julien@gmail.com>
2019-10-30 17:16:23 +0100

    homework week2 database save

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Buyer = require('./buyers');
 const PcBuild  = require('./pc_builds');
-const database = require('./database');
-2
+const Database = require('./database');
+const multer = require('multer');
+
 const Project_Rally = new PcBuild("project_rally","gaming","Amd Ryzen5", "Radeon Rx590", "AsRock B450M", "16Gb", 2019, "1Tb", "dvdrw", "450W", "coolermaster", 700, [] )
 const Cecile = new Buyer("Cecile","Maluma","baby","test@ymail.com",2019,"march", "Berlin") 
 
@@ -10,6 +11,12 @@ Cecile.printLocation();
 Cecile.addToWishlist(Project_Rally);
 
 Project_Rally.printBuyersName();
+
+Database.save('pcbuild.json', Project_Rally);
+
+const loadedFile = Database.load('pcbuild.json')
+console.log(loadedFile.name)
+
 
 
 
